fc24e214b56d407ca7f0ec0b0f318d4ced45203a
Jeff Betz <jeffbetz@gmail.com>
2012-12-10 15:14:25 -0600

    implementing sftp

--- scripts/88nine.js
+++ scripts/88nine.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($) {
 
 	// John, this will add an ellipsis to the end of a paragraph or line if text overflows
 	$(".addEllipsis").ellipsis();
+	//test
 
 	// $(".programListing:last-child").css({ border-bottom: none; });
 });
