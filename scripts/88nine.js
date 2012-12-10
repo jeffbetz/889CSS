402dd6878d26e6fc9feb6077ad87dc591d4f2f24
Jeff Betz <jeffbetz@gmail.com>
2012-12-09 23:13:13 -0600

    program listing and music player

---
+++ scripts/88nine.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+
+	// John, this will add an ellipsis to the end of a paragraph or line if text overflows
+	$(".addEllipsis").ellipsis();
+
+	// $(".programListing:last-child").css({ border-bottom: none; });
+});
