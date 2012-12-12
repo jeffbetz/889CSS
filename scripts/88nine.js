74f5f7c204f31ddae9e86cf0424dbca61b34dc28
Jeff Betz <jeffbetz@gmail.com>
2012-12-11 22:43:37 -0600

    made expandable ad

--- scripts/88nine.js
+++ scripts/88nine.js
@@ -4,5 +4,31 @@ jQuery(document).ready(function($) {
 	$(".addEllipsis").ellipsis();
 	//test
 
+	// $(".expandButton").click(function() {
+	// 	$(this).slideUp();
+	//   $(".expandMegaAd").css({ height: 440px; })
+	// });
+
+	  // $(".expandButton").click(function(){
+	  //   $(this).fadeOut();
+	  //   $(".expandMegaAd").css('height','440px')
+	  // });
+
+	  $(".expandButton").click(function(){
+		    $(this).fadeOut();
+		    $(".closeButton").fadeIn();
+
+		    // $(".expandMegaAd").animate({height: "440px"}, { duration: "1000" });
+		    $(".expandMegaAd").css({height: "440px"});
+	  });
+
+	  $(".closeButton").click(function(){
+		    $(this).fadeOut();
+		    $(".expandButton").fadeIn();
+
+		    // $(".expandMegaAd").animate({height: "440px"}, { duration: "1000" });
+		    $(".expandMegaAd").css({height: "120px"});
+	  });
+
 	// $(".programListing:last-child").css({ border-bottom: none; });
 });
